2351c5169c9e8e581b92cd7e891da1a6f0935dbd
Oscar Castañeda <ocastaneda@digitalactive.co>
2020-11-30 00:54:34 -0500

    Updates

---
+++ precache-manifest.3d38f79126d5b8e9773d369d4953f73a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "356acb9576eadf3437b2882ef28266be",
+    "url": "/crm-test/index.html"
+  },
+  {
+    "revision": "e755e2d5ac48f2f285cb",
+    "url": "/crm-test/static/css/main.003ab658.chunk.css"
+  },
+  {
+    "revision": "460c05a89d2a5aa25bc7",
+    "url": "/crm-test/static/js/2.0e784da5.chunk.js"
+  },
+  {
+    "revision": "28f29a598e465553cfeea810767399d3",
+    "url": "/crm-test/static/js/2.0e784da5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e755e2d5ac48f2f285cb",
+    "url": "/crm-test/static/js/main.25cfbbf5.chunk.js"
+  },
+  {
+    "revision": "36a1a00470c380d3fd6e",
+    "url": "/crm-test/static/js/runtime-main.394f4472.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/crm-test/precache-manifest.3d38f79126d5b8e9773d369d4953f73a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/crm-test/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
